783c618891b0696612579d088c490d004dfe7071
HugoFredriksson <89245482+HugoFredriksson@users.noreply.github.com>
2021-10-08 14:07:19 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+function chad() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "jew.gif");
+    x.setAttribute("width", "500");
+    x.setAttribute("height", "250");
+    x.setAttribute("alt", "en gasig individ");
+    x.setAttribute("id", "ba");
+    document.body.appendChild(x);
+    document.getElementById("ba").remove();
+}
+function bröd() {
+    var y = document.createElement("IMG");
+    y.setAttribute("src", "troll.gif");
+    y.setAttribute("width", "500");
+    y.setAttribute("height", "200");
+    y.setAttribute("alt", "en vacker individ");
+    y.setAttribute("id", "da");
+    document.body.appendChild(y);
+    document.getElementById("da").remove();
+}
+function marko() {
+    var z = document.createElement("IMG");
+    z.setAttribute("src", "marko.jpg");
+    z.setAttribute("width", "500");
+    z.setAttribute("height", "250");
+    z.setAttribute("alt", "en makaronig individ");
+    z.setAttribute("id", "va");
+    document.body.appendChild(z);
+    document.getElementById("va").remove();
+}
